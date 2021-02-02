34541a389fe325350f18b2652d097f1f01798bda
YULitle <71101155+YULitle@users.noreply.github.com>
2021-02-02 11:29:41 -0600

    Update README.m

--- README.m
+++ README.m
@@ -1,3 +1,5 @@
 The "frequency" doesn't match actualy frequency. It is inverted. Also, the frame rate messes with the appearance of the frequency, of course. But I plan on setting the frame rate at some high value and then only allowing the frequency to change.
 
 I'd like to add a color coding where the color matched the frequency of light.
+
+[Main Page](https://yulitle.github.io/DopplerEffectDemonstration)
